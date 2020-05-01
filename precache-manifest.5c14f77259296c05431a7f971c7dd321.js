e30d133ae20f2d400b303816675cecbe3ea95bbf
deepanshu-kav <62233903+deepanshu-kav@users.noreply.github.com>
2020-05-01 19:07:35 +0530

    Updates

---
+++ precache-manifest.5c14f77259296c05431a7f971c7dd321.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfabe943aefbba102ac7062470f3f87f",
+    "url": "/to-do-list/index.html"
+  },
+  {
+    "revision": "ce4315a65b0a1523a92d",
+    "url": "/to-do-list/static/css/main.51cf257e.chunk.css"
+  },
+  {
+    "revision": "c89e74c3fec577661e5d",
+    "url": "/to-do-list/static/js/2.bb97dd66.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/to-do-list/static/js/2.bb97dd66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce4315a65b0a1523a92d",
+    "url": "/to-do-list/static/js/main.cc1254d9.chunk.js"
+  },
+  {
+    "revision": "2cd6034b9fe0623b4841",
+    "url": "/to-do-list/static/js/runtime-main.51f95a8f.js"
+  }
+]);
